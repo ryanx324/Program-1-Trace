fe5cdb29fdee70b8e748205ac4b71e75803c95e3
ryanx324 <rnguye65@calpoly.edu>
2024-01-16 20:59:49 -0800

    open the packet

--- trace.c
+++ trace.c
@@ -3,6 +3,18 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
+    if (argc != 2){
+        fprintf(stderr, "Error: Not the correct amount of args\n", argv[0]); // Error if there is an incorrect amount of arguments
+        return 1;
+    }
+
+    char ERRBUF[PCAP_ERRBUF_SIZE]; // Error buffer of size defined by the header
+
+    pcap_t *handle_packet = pcap_open_offline(argv[1], ERRBUF);
+    if (handle_packet == NULL){
+        fprintf(stderr, "Failed to open file %s: %s\n", argv[1], ERRBUF); // Error if file is unable to open
+        return 2;
+    }
 
     return 0;
 }
