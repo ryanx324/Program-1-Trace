e741134122c1fb5a0969a2949ffdc4c372710078
ryanx324 <rnguye65@calpoly.edu>
2024-01-17 17:24:07 -0800

    printinggit add trace.c!

--- trace.c
+++ trace.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[]){
     if (argc != 2){
-        fprintf(stderr, "Error: Not the correct amount of args\n", argv[0]); // Error if there is an incorrect amount of arguments
+        fprintf(stderr, "Error: Not the correct amount of args\n"); // Error if there is an incorrect amount of arguments
         return 1;
     }
 
@@ -25,15 +25,43 @@ int main(int argc, char *argv[]){
     struct ethernet_header {
         unsigned char dest_addr[6]; // Destination MAC Address
         unsigned char source_addr[6]; // Source MAC Address
-        unsigned int type[2]; // Type
+        unsigned short type; // Type
     };
 
     while ((output = pcap_next_ex(packet_handler, &header, &packet)) == 1){
         struct ethernet_header *eth_hdr = (struct ethernet_header*) packet;
 
+        printf("Ethernet\n");
+
+        printf("Destination MAC: ");
+        for (int i = 0; i < 6; i++){
+            printf("%02x", eth_hdr->dest_addr[i]);
+            if (i < 6 - 1){
+                printf(":");
+            }
+        }
+
+        printf("\n");
+
+        printf("Source MAC: ");
+        for (int i = 0; i < 6; i++){
+            printf("%02x", eth_hdr->source_addr[i]);
+            if (i < 6 - 1){
+                printf(":");
+            }
+        }
+        
+        printf("\n");
+        printf("Type: %04x", ntohs(eth_hdr->type));
+        printf("\n");
+
         if (output == -1){
             fprintf(stderr, "Packet read error\n");
             break;
+        }
+
+        if (output == -2){
+            fprintf(stderr, "Error: EOF\n");
         }
     }
 
