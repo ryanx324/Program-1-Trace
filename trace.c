0b3827abdbc7ec20eebf9ea832ec8e1c0055c170
ryanx324 <rnguye65@calpoly.edu>
2024-01-18 18:00:51 -0800

    yayyy new format

--- trace.c
+++ trace.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <string.h>
+#include <netinet/ether.h>
+#include <netinet/in.h>
 
 int main(int argc, char *argv[]){
     if (argc != 2){
@@ -32,6 +34,10 @@ int main(int argc, char *argv[]){
     // Struct for ARP Header
     struct arp_header {
         char opcode; // Opcode
+        unsigned char hard_type[2]; // Hard type
+        unsigned char prot_type[2]; // Prot type
+        unsigned char hard_size[1]; // Hard size
+        unsigned char prot_size[1]; // Prot size
         unsigned char senderMAC[6]; // Sender MAC address
         unsigned char senderIP[4]; // Sender IP address
         unsigned char targetMAC[6]; // Target MAC address
@@ -45,29 +51,37 @@ int main(int argc, char *argv[]){
         // Reading the ethernet header
         struct ethernet_header *eth_hdr = (struct ethernet_header*) packet;
 
+        char dest_MAC_addr[18];
+        char src_MAC_addr[18];
+
+        memcpy(dest_MAC_addr, ether_ntoa((struct ether_addr*)&eth_hdr->dest_addr), sizeof(dest_MAC_addr));
+        memcpy(src_MAC_addr, ether_ntoa((struct ether_addr*)&eth_hdr->source_addr), sizeof(src_MAC_addr));
+
         printf("Packet Number: %d  Frame Len: %d\n", packet_num, header->len); // Print packet num and frame len
 
         printf("\n");
 
         printf("\tEthernet Header\n"); // Print Eth header
 
-        printf("\t\tDest MAC: "); 
-        for (int i = 0; i < 6; i++){
-            printf("%02x", eth_hdr->dest_addr[i]); // Print Dest MAC addr
-            if (i < 6 - 1){
-                printf(":");
-            }
-        }
+        printf("\t\tDest MAC: %s\n", dest_MAC_addr);
+
+        // printf("\t\tDest MAC: "); 
+        // for (int i = 0; i < 6; i++){
+        //     printf("%01x", eth_hdr->dest_addr[i]); // Print Dest MAC addr
+        //     if (i < 5){
+        //         printf(":");
+        //     }
+        // }
 
         printf("\n");
 
-        printf("\t\tSource MAC: ");
-        for (int i = 0; i < 6; i++){
-            printf("%02x", eth_hdr->source_addr[i]); // Print Source MAC addr
-            if (i < 6 - 1){
-                printf(":");
-            }
-        }
+        // printf("\t\tSource MAC: ");
+        // for (int i = 0; i < 6; i++){
+        //     printf("%01x", eth_hdr->source_addr[i]); // Print Source MAC addr
+        //     if (i < 5){
+        //         printf(":");
+        //     }
+        // }
         
         printf("\n");
         printf("\t\tType: ");
@@ -82,10 +96,41 @@ int main(int argc, char *argv[]){
             // printf("%04x\n", eth_hdr->type);
         }
 
-        printf("\n");
-        printf("\t ARP Header\n");
+        // printf("\n");
 
-        printf("\t\tSender MAC: ");
+        // struct arp_header *arp_hdr = (struct arp_header*) packet;
+        // printf("\t ARP Header\n");
+
+        // printf("\t\tOpcode: ");
+        // printf("\n");
+
+
+        // printf("\t\tSender MAC: ");
+        // for (int i = 22; i < 26; i++){
+        //     printf("%01x", arp_hdr->senderMAC[i]); // Print sender MAC addr
+        //     if (i < 25){
+        //         printf(":");
+        //     }
+        // }
+        // printf("\n");
+        // printf("\t\tSender IP: ");
+        // for (int i = 22; i < 26; i++){
+        //     printf("%d", arp_hdr->senderIP[i]); // Print sender IP addr
+        //     if (i < 25){
+        //         printf(".");
+        //     }
+        // }
+        // printf("\n");
+        // printf("\t\tTarget MAC: ");
+        // printf("\n");
+        // printf("\t\tTarget IP: ");
+        // for (int i = 31; i < 35; i++){
+        //     printf("%d", arp_hdr->senderIP[i]); // Print sender IP addr
+        //     if (i < 34){
+        //         printf(".");
+        //     }
+        // }
+        // printf("\n");
 
         if (output == -1){
             fprintf(stderr, "Packet read error\n");
